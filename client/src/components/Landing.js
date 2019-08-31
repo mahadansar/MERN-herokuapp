6ca09ce21bbf29dd81b21e727d1341a677e48221
Mahad Ansar <46062897+mahadansar@users.noreply.github.com>
2019-09-01 01:53:59 +0500

    client side of app

---
+++ client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Landing = () => {
+    return(
+        <div style={{textAlign: 'center'}}>
+            <h1>
+                Whalecum!
+            </h1>
+            Buy legal illegal stuff here.
+        </div>
+    )
+}
+
+export default Landing;
